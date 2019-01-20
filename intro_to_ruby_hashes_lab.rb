760ff8700883e8402226f3d7e032039be79e5694
Emily Wagner <emilywagner598@gmail.com>
2019-01-20 00:09:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,8 @@ def monopoly_with_third_tier
 				:shortline=>{}
 			}
 	   }
-  }end
+  }
+end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
