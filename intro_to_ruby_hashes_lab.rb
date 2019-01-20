6c8da6f92a883d51aab1e4976b3223fc694f6e7d
Emily Wagner <emilywagner598@gmail.com>
2019-01-20 00:02:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,12 +14,17 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {}
-	monopoly = {
-		:railroads => {:pieces => 4},
-		:names => {},
-		:rent_in_dollars => {}
-	}
+	monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {},
+    :names=>
+    {}
+    }
+  }
+}
 end
 
 def monopoly_with_third_tier
